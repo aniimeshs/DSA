2ad0670a1de74740b64931d08a655b0678625d28
Animesh Singh <animeshpr152002@gmail.com>
2024-02-21 12:25:55 +0530

    Time: 4 ms (79.27%), Space: 9 MB (8.3%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -3,8 +3,8 @@ public:
     int rangeBitwiseAnd(int left, int right) {
         int cnt = 0;
         while (left != right) {
-            left >> = 1;
-            right >> = 1;
+            left >>= 1;
+            right >>= 1;
             cnt++;
         }
         return (left << cnt);
