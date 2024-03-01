1af6369d7d787fe361a8759edf7329e8a4e6bce4
Animesh Singh <animeshpr152002@gmail.com>
2024-03-02 01:49:41 +0530

    Time: 0 ms (100%), Space: 8.3 MB (64.03%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        int cnt1 = 0;
-        int cnt2 = 0;
+        // int cnt1 = 0;
+        // int cnt2 = 0;
 
-        for(int i=0; i<s.length(); i++){
-            if(s[i] == '1'){
-                cnt1++;
-            }
+        // for(int i=0; i<s.length(); i++){
+        //     if(s[i] == '1'){
+        //         cnt1++;
+        //     }
+        //     else{
+        //         cnt2++;
+        //     }
+        // }
+        // //strins ans.append(cnt1-1,'1)
+        // string ans = string(cnt1-1, '1') + string(cnt2, '0') + '1';
+        // return ans;
+        int st = 0;
+        int e = s.size()-1;
+
+        while(st<=e){
+            if(s[st] == '1') 
+                st++;
+            else if(s[e] == '0')
+                e--;
             else{
-                cnt2++;
+                swap(s[st++], s[e--]);
             }
         }
-        //strins ans.append(cnt1-1,'1)
-        string ans = string(cnt1-1, '1') + string(cnt2, '0') + '1';
-        return ans;
+        swap(s[e], s[s.size()-1]);
+        return s;
     }
 };
