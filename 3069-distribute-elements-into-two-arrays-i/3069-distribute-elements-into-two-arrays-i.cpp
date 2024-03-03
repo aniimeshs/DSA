b24524cd6642808863e359509b5c34f5f896ec53
Animesh Singh <animeshpr152002@gmail.com>
2024-03-03 11:45:13 +0530

    Time: 5 ms (60%), Space: 22.9 MB (90%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    vector<int> resultArray(vector<int>& nums) {
-        stack<int>st1;
-        stack<int>st2;
+    // vector<int> resultArray(vector<int>& nums) {
+    //     stack<int>st1;
+    //     stack<int>st2;
 
-        int i = 0;
+    //     int i = 0;
 
-        st1.push(nums[i++]);
-        st2.push(nums[i++]);
+    //     st1.push(nums[i++]);
+    //     st2.push(nums[i++]);
 
-        while(i < nums.size()){
-            if(st1.top() > st2.top()){
-                st1.push(nums[i++]);
-            }else{
-                st2.push(nums[i++]);
-            }
-        }
+    //     while(i < nums.size()){
+    //         if(st1.top() > st2.top()){
+    //             st1.push(nums[i++]);
+    //         }else{
+    //             st2.push(nums[i++]);
+    //         }
+    //     }
         
-        i = nums.size()-1;
-        while(!st2.empty()){
-            nums[i--] = st2.top();
-            st2.pop();
+    //     i = nums.size()-1;
+    //     while(!st2.empty()){
+    //         nums[i--] = st2.top();
+    //         st2.pop();
+    //     }
+    //     while(!st1.empty()){
+    //         nums[i--] = st1.top();
+    //         st1.pop();
+    //     }
+    //     return nums;
+    // }
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> vec1(1, nums[0]), vec2(1, nums[1]);
+        for(int i= 2; i < nums.size(); ++i){
+            if(vec1.back() > vec2.back()) 
+                vec1.push_back(nums[i]);
+            else 
+                vec2.push_back(nums[i]);
         }
-        while(!st1.empty()){
-            nums[i--] = st1.top();
-            st1.pop();
-        }
-        return nums;
+        vec1.insert(vec1.end(), vec2.begin(), vec2.end());
+        return vec1;
     }
 };
