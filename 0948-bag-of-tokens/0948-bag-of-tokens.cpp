662250213fb5d33eae00648782d20bec139a070f
Animesh Singh <animeshpr152002@gmail.com>
2024-03-04 14:54:32 +0530

    Time: 0 ms (100%), Space: 13.1 MB (55.08%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -8,15 +8,21 @@ public:
         int score = 0, max_score = 0;
 
         while(s <= e){
+            // Check if the token is greater than power
+            // if token is greater than power then increase the score by 1 and
+            // power is decreased by the token[i]
             if(power >= tokens[s]){
                 power -= tokens[s++];
                 score += 1;
                 max_score = max(score, max_score);
             }
+            // if power is less than token and score is atleast 1 then increase the
+            // power by max token and score is decreased by 1.
             else if(score > 0){
                 power += tokens[e--];
                 score -= 1;
             }
+            // if token is greater than power and score is not even 1
             else{
                 break;
             }
