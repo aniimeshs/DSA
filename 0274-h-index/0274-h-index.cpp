b590eb472d41c1c8826244e87e56228442decece
Animesh Singh <animeshpr152002@gmail.com>
2024-03-04 16:03:04 +0530

    Time: 0 ms (100%), Space: 10.8 MB (15.61%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,13 +3,14 @@ public:
     int hIndex(vector<int>& citations) {
         sort(citations.begin(), citations.end());
         int n = citations.size();
-        int maxi = 0;
-        
-        for(int i = 0; i < n; i++) {
-            if(citations[i] >= n - i) {
-                maxi = max(maxi, n - i);
+        int i = 0, max_h = 0;
+
+        while(i < n){
+            if(n - i <= citations[i]){
+                max_h = max(n - i, max_h);
             }
+            i++;
         }
-        return maxi;
+        return max_h;
     }
 };
