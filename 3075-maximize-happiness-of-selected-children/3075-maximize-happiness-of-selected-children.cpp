8c2aacf7cbfd14ade3bbd36fb98338a6e712bcc6
Animesh Singh <animeshpr152002@gmail.com>
2024-03-10 13:09:31 +0530

    Time: 156 ms (71.43%), Space: 107.1 MB (28.57%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        int cnt = 0; // For happiness value decrement by 1
+        long long value = 0;
+        while(k > 0 && cnt < happiness.size()){
+            if(happiness[cnt] - cnt >= 0) 
+                value += happiness[cnt] - cnt;
+            else 
+                break;
+            cnt++;
+            k--;
+        }
+        return value;
+    }
+};
