9c7639a4dcb6c5dcfd366b9df7d9fe14b5655d15
Animesh Singh <animeshpr152002@gmail.com>
2024-03-10 12:49:09 +0530

    Time: 13 ms (16.67%), Space: 32.1 MB (66.67%) - LeetHub

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
@@ -6,10 +6,10 @@ public:
         int sum = accumulate(apple.begin(), apple.end(), 0);
 
         int cnt = 0;
-        while(sum > 0){
+        while(sum > 0 && cnt <= capacity.size()){
             sum -= capacity[cnt++];
         }
-        
+
         return cnt;
     }
 };
