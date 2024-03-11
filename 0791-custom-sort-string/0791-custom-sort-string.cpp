e597144f01394d12ef3ad59756a4eca92833d91f
Animesh Singh <animeshpr152002@gmail.com>
2024-03-12 00:34:40 +0530

    Time: 0 ms (100%), Space: 7.4 MB (78.2%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        unordered_map<char, int> charCount;
-        for (char c : order) {
-            charCount[c] = 0;
-        }
+        // unordered_map<char, int> charCount;
+        // for (char c : order) {
+        //     charCount[c] = 0;
+        // }
 
-        for (char c : s) {
-            if (charCount.find(c) != charCount.end()) {
-                charCount[c]++;
-            }
-        }
+        // for (char c : s) {
+        //     if (charCount.find(c) != charCount.end()) {
+        //         charCount[c]++;
+        //     }
+        // }
 
-        string str;
-        for (char c : order) {
-            str.append(charCount[c], c); 
-            // Appending character from charCount in str a/c to occurences.
-        }
+        // string str;
+        // for (char c : order) {
+        //     str.append(charCount[c], c); // charCount[c] gives count of characters.
+        //     // Appending character from charCount in str a/c to occurences.
+        // }
 
-        for (char c : s) {
-            if (charCount.find(c) == charCount.end()) {
-                str.push_back(c);
-            }
-        }
+        // for (char c : s) {
+        //     if (charCount.find(c) == charCount.end()) {
+        //         str.push_back(c);
+        //     }
+        // }
         
-        return str;
+        // return str;
+
+        auto cmp = [&](char a,char b){
+            return order.find(a) < order.find(b);
+        };
+        sort(s.begin(),s.end(),cmp);
+        return s;
     }
 };
