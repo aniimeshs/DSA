fef651df243c2a5a90fe4dc9f8a066cee173e07c
Animesh Singh <animeshpr152002@gmail.com>
2024-04-24 13:33:00 +0530

    Time: 2 ms (42.64%), Space: 9.8 MB (45.7%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,14 +11,46 @@
  */
 class Solution {
 public:
-    vector<int>answer;
+    // vector<int>answer;
     
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     if(root != NULL){
+    //         answer.push_back(root -> val);
+    //         preorderTraversal(root -> left);
+    //         preorderTraversal(root -> right);
+    //     }
+    //     return answer;
+    // }
+    
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> nodes;
+    //     preorder(root, nodes);
+    //     return nodes;
+    // }
+// private:
+    // void preorder(TreeNode* root, vector<int>& nodes) {
+    //     if (!root) {
+    //         return;
+    //     }
+    //     nodes.push_back(root -> val);
+    //     preorder(root -> left, nodes);
+    //     preorder(root -> right, nodes);
+    // }
     vector<int> preorderTraversal(TreeNode* root) {
-        if(root != NULL){
-            answer.push_back(root -> val);
-            preorderTraversal(root -> left);
-            preorderTraversal(root -> right);
+        vector<int>arr;
+        if(root == NULL) return arr;
+
+        stack<TreeNode*>st;
+        st.push(root);
+
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            arr.push_back(root->val);
+
+            if(root -> right != NULL) st.push(root -> right);
+            if(root -> left != NULL) st.push(root -> left);
         }
-        return answer;
+        return arr;
     }
 };
