11b632d6f57369bd144fcaa55904c9f4145e1363
Animesh Singh <animeshpr152002@gmail.com>
2024-04-29 00:30:33 +0530

    Time: 6 ms (21.78%), Space: 10.9 MB (28.34%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool>result;
+        auto maxi = max_element(candies.begin(), candies.end());
+        for(int i = 0; i < candies.size(); i++){
+            if((candies[i] + extraCandies) >= *maxi)
+                result.push_back(true);
+            else
+                result.push_back(false);
+        }
+
+        return result;
+    }
+};
