1f46ee27fd9e8308c29e1b9ca47c8ee389c456a6
Animesh Singh <animeshpr152002@gmail.com>
2024-05-08 09:36:11 +0530

    Time: 0 ms (100%), Space: 13.8 MB (70.05%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int,int>>q;
+        for(int i=0;i<size(score);i++){
+            q.push({score[i],i});
+        }
+
+        int i=0;
+        vector<string>ans(size(q));
+        while(!q.empty()){
+            ans[q.top().second]=i==0?"Gold Medal":i==1?"Silver Medal":i==2?
+            "Bronze Medal":to_string(i+1);
+            i++;
+            q.pop();
+        }
+
+        return ans;
+    }
+};
