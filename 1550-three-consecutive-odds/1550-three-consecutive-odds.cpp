ecc3f4e5482371bfb3da498a5027feb0db02ce41
Animesh Singh <animeshpr152002@gmail.com>
2024-07-01 17:47:17 +0530

    Time: 3 ms (60.88%), Space: 10.7 MB (8.09%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -1,18 +1,20 @@
+
 class Solution {
 public:
-    bool odd(int n) {
-        return n % 2 != 0;
-    }
-
     bool threeConsecutiveOdds(vector<int>& arr) {
-        if (arr.size() < 3) return false; 
-
-        for (int i = 0; i <= arr.size() - 3; ++i) {
-            if (odd(arr[i]) && odd(arr[i + 1]) && odd(arr[i + 2])) {
-                return true;
-            }
-        }
-        
+        if(arr.size()==1||arr.size()==2)
         return false;
+        for(int i=0;i<arr.size()-2;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                if(arr[i+1]%2 !=0 && arr[i+2]%2 !=0)
+                {
+                   return true;
+                }
+            }
+
+        }
+      return false;  
     }
 };
