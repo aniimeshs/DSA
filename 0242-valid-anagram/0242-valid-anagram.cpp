9b6701e51145ecf92320686e85d0fdd858656971
Animesh Singh <animeshpr152002@gmail.com>
2024-11-21 14:45:40 +0530

    Time: 7 ms (37.66%), Space: 8.6 MB (98.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int table[256] = {0};
+        if(s.length() != t.length()) return false;
+        
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
 
-        for(int i=0; i<s.size(); i++){
-            table[s[i]]++;
-        }
-
-        for(int i=0; i<t.size(); i++){
-            table[t[i]]--;
-        } 
-
-        for(int i=0; i<256; i++){
-            if(table[i] != 0){
-                return false;
-            }
-        }
-        return true;
+        return s == t;
     }
 };
