a3c0bda87d5fc3476619252f5207d61419d2da06
Animesh Singh <animeshpr152002@gmail.com>
2024-11-21 15:04:34 +0530

    Time: 3 ms (78.97%), Space: 14.4 MB (20.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,26 +1,13 @@
-/*
-    Given int array & target, return indices of 2 nums that add to target
-    Ex. nums = [2,7,11,15] & target = 9 -> [0,1], 2 + 7 = 9
-
-    At each num, calculate complement, if exists in hash map then return
-
-    Time: O(n)
-    Space: O(n)
-*/
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        unordered_map<int, int> mp; // val -> index
-
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (mp.find(complement) != mp.end()) {
-                return {mp[complement], i};
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++){
+            if (mp.find(target-nums[i]) != mp.end()){
+                return {mp[target-nums[i]], i};
             }
-            mp.insert({nums[i], i});
+            mp[nums[i]] = i;
         }
-        return {};
+        return {0, 0};
     }
 };
