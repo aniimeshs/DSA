8af772128b384361485d2f4a6cd96ad33337e6bf
Animesh Singh <animeshpr152002@gmail.com>
2025-02-06 15:00:57 +0530

    Time: 320 ms (53.6%), Space: 84.9 MB (81.5%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -7,10 +7,15 @@ public:
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
                 int product = nums[i] * nums[j];
-                ans += 8 * checkMap[product];
+                // ans += 8 * checkMap[product];
                 checkMap[product]++;
             }
         }
+        for(pair<int,int> m:checkMap){
+            int freq = m.second;
+            ans += (freq * (freq - 1)) / 2; //no. of tuple
+        }
+        return ans * 8;
 
         return ans;
     }
