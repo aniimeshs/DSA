7117e6351c57563cee456a6207df218344d5d1cf
Animesh Singh <animeshpr152002@gmail.com>
2025-02-09 20:28:43 +0530

    Time: 0 ms (100%), Space: 32 MB (73.84%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-        int m = score.size();
-        int n = score[0].size();
+        // int m = score.size();
+        // int n = score[0].size();
 
-        priority_queue<pair<int, int>>pq;
-        for(int i = 0; i < m; i++) {
-            pq.push({score[i][k], i});
-        }
+        // priority_queue<pair<int, int>>pq;
+        // for(int i = 0; i < m; i++) {
+        //     pq.push({score[i][k], i});
+        // }
 
-        vector<vector<int>> ans;
-        while(!pq.empty()) {
-            int row = pq.top().second;
-            pq.pop();
-            ans.push_back(score[row]);
-        }
+        // vector<vector<int>> ans;
+        // while(!pq.empty()) {
+        //     int row = pq.top().second;
+        //     pq.pop();
+        //     ans.push_back(score[row]);
+        // }
 
-        return ans;
+        // return ans;
+
+        sort(score.begin(), score.end(),[&](const vector<int>& row1, const vector<int>& row2){
+            return row1[k]>row2[k];
+        });
+        
+        return score;
     }
 };
