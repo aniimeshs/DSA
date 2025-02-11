b107ac0b2db5c2bf1fca87528aeeec8a0ba63074
Animesh Singh <animeshpr152002@gmail.com>
2025-02-11 19:37:28 +0530

    Time: 7 ms (21.89%), Space: 33.9 MB (93.44%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> findMatrix(vector<int>& nfreqs) {
-        unordered_map<int, int> freq;
-        for (int nfreq : nfreqs) {
-            freq[nfreq]++;
-        }
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<int> freq(nums.size() + 1);
 
         vector<vector<int>> ans;
-        while (!freq.empty()) {
-            vector<int> temp, toErase;
-            for (auto &[key, count] : freq) {
-                temp.emplace_back(key);
-                count--;
-                if (count == 0)toErase.emplace_back(key);
+        for (int c : nums) {
+            if (freq[c] >= ans.size()) {
+                ans.push_back({});
             }
-            ans.emplace_back(temp);
-            for (auto &i : toErase){
-                freq.erase(i);
-            }
+
+            ans[freq[c]].push_back(c);
+            freq[c]++;
         }
+
         return ans;
     }
 };
