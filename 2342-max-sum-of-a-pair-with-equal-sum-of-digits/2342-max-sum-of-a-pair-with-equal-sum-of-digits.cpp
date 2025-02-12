868bbb3aca0f27444b506e46af4b6f7785a81b0f
Animesh Singh <animeshpr152002@gmail.com>
2025-02-12 14:28:53 +0530

    Time: 5 ms (96.25%), Space: 63.6 MB (98.5%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     int maximumSum(vector<int>& nums) {
-        unordered_map<int, priority_queue<int>> digitSum;
-        for(int num : nums) {
-            int sum = 0, temp = num;
-            while(temp) {
+        // unordered_map<int, priority_queue<int>> digitSum;
+        // for(int num : nums) {
+        //     int sum = 0, temp = num;
+        //     while(temp) {
+        //         sum += temp % 10;
+        //         temp /= 10;
+        //     }
+        //     digitSum[sum].push(num);
+        // }
+
+        // int ans = -1;
+        // for(auto& it : digitSum) {
+        //     if(it.second.size() > 1) {
+        //         int sum = it.second.top();
+        //         it.second.pop();
+        //         sum += it.second.top();
+        //         ans = max(ans, sum);
+        //     }
+        // }
+        // return ans;
+
+        int max[82] = {0};
+        int ans = -1;
+        for (int x : nums) {
+            int sum = 0, temp = x;
+            while (temp != 0) {
                 sum += temp % 10;
                 temp /= 10;
             }
-            digitSum[sum].push(num);
-        }
-
-        int ans = -1;
-        for(auto& it : digitSum) {
-            if(it.second.size() > 1) {
-                int sum = it.second.top();
-                it.second.pop();
-                sum += it.second.top();
-                ans = max(ans, sum);
-            }
+            if (max[sum] != 0) ans = std::max(ans, x + max[sum]);
+            max[sum] = std::max(max[sum], x);
         }
         return ans;
     }
