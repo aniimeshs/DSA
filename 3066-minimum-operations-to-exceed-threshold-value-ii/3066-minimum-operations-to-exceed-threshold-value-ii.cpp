0ac53cdd893332ee6f1b7fd3dc11cbbfe5f968b8
Animesh Singh <animeshpr152002@gmail.com>
2025-02-13 11:03:21 +0530

    Time: 88 ms (86.21%), Space: 91.4 MB (90.45%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        priority_queue<long long, vector<long long>, greater<long long>>
-        pq (nums.begin(), nums.end()); 
+        // priority_queue<long long, vector<long long>, greater<long long>>
+        // pq (nums.begin(), nums.end()); 
+        // int operations = 0;
+
+        // while (pq.size() > 1 && pq.top() < k) {
+        //     long long x = pq.top(); pq.pop();
+        //     long long y = pq.top(); pq.pop();
+
+        //     long long newElement = min(x, y) * 2LL + max(x, y);
+        //     pq.push(newElement);
+        //     operations++;
+        // }
+
+        // return operations;
+        vector<long long> heap(nums.begin(), nums.end());
+        make_heap(heap.begin(), heap.end(), greater<long long>());
         int operations = 0;
 
-        while (pq.size() > 1 && pq.top() < k) {
-            long long x = pq.top(); pq.pop();
-            long long y = pq.top(); pq.pop();
+        while (heap.size() > 1 && heap.front() < k) {
+            pop_heap(heap.begin(), heap.end(), greater<long long>());
+            long long x = heap.back(); heap.pop_back();
 
-            long long newElement = min(x, y) * 2LL + max(x, y);
-            pq.push(newElement);
+            pop_heap(heap.begin(), heap.end(), greater<long long>());
+            long long y = heap.back(); heap.pop_back();
+
+            heap.push_back(x * 2 + y);
+            push_heap(heap.begin(), heap.end(), greater<long long>());
             operations++;
         }
 
